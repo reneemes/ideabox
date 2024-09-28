d17e5516738dcb8085728eb9fb160f52e2777d7c
Renee Messersmith <morganrmessersmith@gmail.com>
2024-09-27 20:22:02 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Ideas from './Ideas';
+// import Card from './Card';
+import Form from './Form';
+import { useState } from 'react';
+
 
 function App() {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+]
+  const [ideas, setIdeas] = useState([]);
+
+  function addIdea (newIdea) {
+    setIdeas([...ideas, newIdea])
+  }
+
+  function deleteIdea(id) {
+    console.log(id);
+    const filteredIdeas = ideas.filter(idea => idea.id !== id)
+    setIdeas(filteredIdeas)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+      <h1>IdeaBox</h1>
+      { !ideas.length && <h2>No ideas yet -- add some!</h2> }
+      <Form addIdea={addIdea} />
+      <Ideas ideas={ideas} deleteIdea={deleteIdea}/>
+    </main>
+  )
+
+  
 }
 
 export default App;
